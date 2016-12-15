8507d2251985502c51384c851ef98ed2531c0d95
eyllanesc <e.yllanescucho@gmail.com>
2016-12-14 22:05:00 -0500

    update

--- RENIPRESS-selenium/RENIPRESS_selenium.py
+++ RENIPRESS-selenium/RENIPRESS_selenium.py
@@ -1,9 +1,11 @@
 import glob
 import os
 import shutil
+import time
 from time import sleep
-
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def clear(directory):
@@ -18,34 +20,30 @@ def clear(directory):
             os.remove(directory + "/" + file)
 
 
-def save(input_dir, output_dir):
+def save(input_dir, output_dir, number, timeout=50):
     """
     Mueve los archivos y los enumera: {numero}.xls
     :param input_dir: directorio donde se encuentra el archivo .xls
     :param output_dir: directorio donde se movera el archivo
+    :param name: nombre del archivo
     :return:
     """
+    counter = 0
     # espera que exista el archivo, ya que la descarga demora
-    while len(glob.glob(input_dir + "/*.xls")) == 0:
+    while (len(glob.glob(input_dir + "/*.xls")) == 0) and (counter < timeout):
         print("waiting")
         sleep(1)
+        counter += 1
+
+    if counter == timeout:
+        return
 
     # creamos la carpeta sino existe
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # se asume que los archivos estan numerados y tienen el formaro {numero}.xls
-    counters = [int(file.replace(".xls", "")) for file in os.listdir(output_dir)]
-
-    # nombre por defecto(0.xls) si la carpeta esta vacia
-    name = 0
-
-    # sino sera uno más que el mayor numero
-    if counters:
-        name = max(counters) + 1
-
     for src in glob.glob(input_dir + "/*.xls"):
-        dst = output_dir + "/" + str(name) + ".xls"
+        dst = output_dir + "/" + str(number) + ".xls"
         print(dst)
         # mueve el archivo hacia su destino con el nuevo nombre
         shutil.move(src, dst)
@@ -62,7 +60,26 @@ if __name__ == '__main__':
           "?action=mostrarBuscar#"
     chrome = webdriver.Chrome(chromedriver)
     chrome.get(url)
-    chrome.execute_script("document.getElementById('datable-grilla-establecimientos-renipress_btnExpAux').click();")
+    time.sleep(5)  # Let the user actually see something! (5 seconds)
+    # Filtering
+    print("Filtering")
+    elem_estado = chrome.find_element_by_xpath('//*[@id="cmb_estado"]/option[1]')
+    elem_estado.click()  # para tener a eess activos y no activos
+    print("clicked")
+    numbers = list(range(2, 482))
+    numbers.remove(24)
+    for number in numbers:
+        print(number)
+        elem_servicio = chrome.find_element_by_xpath(
+            '//*[@id="div-cabecera-buscar"]/div[2]/div[1]/div[3]/div[3]/div/select/option[{number}]'.format(number=number))
+        elem_servicio.click()
+        print(number)
 
-    save(location, output_dr)
-    chrome.quit()
+        elem_buscar = chrome.find_element_by_xpath('//*[@id="btnBuscar"]/i')
+        elem_buscar.click()  # para tener mostrar la base seleccionada
+
+        time.sleep(5)  # Let the user actually see something! (5 seconds)
+        chrome.execute_script("document.getElementById('datable-grilla-establecimientos-renipress_btnExpAux').click();")
+        save(location, output_dr, number)
+
+    # chrome.quit()
