695151b110ec94fcacf76335c67a95b994b7de1a
eyllanesc <e.yllanescucho@gmail.com>
2017-03-01 03:07:49 -0500

    camera

---
+++ Camera/mainwindow.cpp
@@ -0,0 +1,42 @@
+﻿#include "mainwindow.h"
+
+#include <QCameraInfo>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent)
+{
+
+    QWidget* centralWidget = new QWidget(this);
+    this->setCentralWidget( centralWidget );
+     layout = new QHBoxLayout( centralWidget );
+
+    m_QPushButton_calibration = new QPushButton("Calibrate", this);
+    connect(m_QPushButton_calibration, SIGNAL (released()),this, SLOT (handleButton()));
+
+    QList<QCameraInfo> l_QListQCameraInfo_available_cameras = QCameraInfo::availableCameras();
+    if (l_QListQCameraInfo_available_cameras.length() > 2)
+    {
+        m_QCamera_required_camera = new QCamera (l_QListQCameraInfo_available_cameras[1]);
+    }
+    else
+        m_QCamera_required_camera = new QCamera(l_QListQCameraInfo_available_cameras[0]);
+
+    QCameraViewfinder *mCameraViewfinder = new QCameraViewfinder(this);
+    m_QCamera_required_camera->setViewfinder(mCameraViewfinder);
+
+    m_QCamera_required_camera->start();
+
+    layout->addWidget(m_QPushButton_calibration);
+    layout->addWidget(mCameraViewfinder);
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::handleButton()
+{
+
+}
---
+++ Camera/mainwindow.h
@@ -0,0 +1,29 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QCamera>
+#include <QHBoxLayout>
+#include <QCameraViewfinder>
+#include <QCameraImageCapture>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    QPushButton *m_QPushButton_calibration;
+    QCamera *m_QCamera_required_camera;
+    QHBoxLayout* layout;
+
+public slots:
+    void handleButton();
+
+};
+
+#endif // MAINWINDOW_H
