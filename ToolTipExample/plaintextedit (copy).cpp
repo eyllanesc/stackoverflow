a0ac7240c65431a1d79754421292e1a4e34056a1
eyllanesc <e.yllanescucho@gmail.com>
2017-03-08 13:04:33 -0500

    files

---
+++ ToolTipExample/plaintextedit (copy).cpp
@@ -0,0 +1,24 @@
+﻿#include "plaintextedit.h"
+#include <QToolTip>
+
+
+PlainTextEdit::PlainTextEdit(QWidget *parent):QPlainTextEdit(parent)
+{
+    setMouseTracking(true);
+}
+
+bool PlainTextEdit::event(QEvent *event)
+{
+    if (event->type() == QEvent::ToolTip)
+    {
+        QHelpEvent* helpEvent = static_cast<QHelpEvent*>(event);
+        QTextCursor cursor = cursorForPosition(helpEvent->pos());
+        cursor.select(QTextCursor::WordUnderCursor);
+        if (!cursor.selectedText().isEmpty())
+            QToolTip::showText(helpEvent->globalPos(), cursor.selectedText());
+        else
+            QToolTip::hideText();
+        return true;
+    }
+    return QPlainTextEdit::event(event);
+}
