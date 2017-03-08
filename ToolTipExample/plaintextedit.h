aac71a2b250e09b97f70cde62ae23373cc547f0c
eyllanesc <e.yllanescucho@gmail.com>
2017-03-08 12:59:02 -0500

    files

---
+++ ToolTipExample/plaintextedit.h
@@ -0,0 +1,14 @@
+﻿#ifndef PLAINTEXTEDIT_H
+#define PLAINTEXTEDIT_H
+
+#include <QPlainTextEdit>
+
+class PlainTextEdit : public QPlainTextEdit
+{
+public:
+    PlainTextEdit(QWidget *parent=0);
+
+    bool event(QEvent *event);
+};
+
+#endif // PLAINTEXTEDIT_H
