88cf06f3f7ec3ecccb599d321905dced75bf9f6f
eyllanesc <e.yllanescucho@gmail.com>
2017-06-02 07:13:50 -0500

    ci

---
+++ CircleItem/mycircle.cpp
@@ -0,0 +1,34 @@
+#include "mycircle.h"
+
+MyCircle::MyCircle(QObject *parent) : QObject(parent)
+{
+    rect = QRect(0, 0, 100, 100);
+}
+
+QRect MyCircle::geometry() const
+{
+    return rect;
+}
+
+void MyCircle::setGeometry(const QRect &value)
+{
+    if(rect!=value){
+        rect = value;
+        update();
+    }
+}
+
+QRectF MyCircle::boundingRect() const
+{
+    return QRectF(rect);
+}
+
+
+void MyCircle::paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+           QWidget *widget)
+{
+    Q_UNUSED(option)
+    Q_UNUSED(widget)
+    painter->setBrush(QBrush(Qt::red));
+    painter->drawEllipse(rect);
+}
---
+++ CircleItem/mycircle.h
@@ -0,0 +1,28 @@
+#ifndef MYCIRCLE_H
+#define MYCIRCLE_H
+
+#include <QGraphicsItem>
+#include <QObject>
+#include <QPainter>
+#include <QStyleOptionGraphicsItem>
+
+class MyCircle : public QObject, public QGraphicsItem
+{
+    Q_OBJECT
+    Q_PROPERTY(QRect geometry READ geometry WRITE setGeometry)
+    Q_INTERFACES(QGraphicsItem)
+public:
+    explicit MyCircle(QObject *parent = nullptr);
+
+    QRect geometry() const;
+    void setGeometry(const QRect &value);
+
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,QWidget *widget);
+
+private:
+
+    QRect rect;
+};
+
+#endif // MYCIRCLE_H
