5eb361fd232db7d88a3a66dc76d4accdf5b0dc7a
eyllanesc <e.yllanescucho@gmail.com>
2017-06-01 22:30:20 -0500

    tee

---
+++ TextEditExample/mainwindow.cpp
@@ -0,0 +1,53 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFile>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QTimer>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QLineEdit>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButton, &QPushButton::clicked, [=](){
+       QString fileName = QFileDialog::getOpenFileName(this,
+                 tr("Open XML File 1"), "", tr("XML Files (*.txt)"));
+       ui->lineEdit->setText(fileName);
+        QFile file(fileName);
+        if(!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::critical(this, "error", file.errorString());
+            return;
+        }
+        QTextStream in(&file);
+        while (!in.atEnd()) {
+            texts.append(in.readLine());
+        }
+        timer->start(1000);
+        file.close();
+    });
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, [=](){
+        if(!texts.isEmpty()){
+            ui->textEdit->append(texts.first());
+            texts.pop_front();
+        }
+        else{
+            timer->stop();
+            qDebug()<<"finish";
+        }
+    });
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ TextEditExample/mainwindow.h
@@ -0,0 +1,24 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QStringList>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QStringList texts;
+};
+
+#endif // MAINWINDOW_H
