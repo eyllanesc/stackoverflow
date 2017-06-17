a54ab1b6e8b35f4faf54598ac859a2835250efc0
eyllanesc <e.yllanescucho@gmail.com>
2017-06-17 08:22:46 -0500

    pandas

---
+++ PandasTableView/main.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.8.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(512, 435)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pathLE = QtWidgets.QLineEdit(self.centralwidget)
+        self.pathLE.setObjectName("pathLE")
+        self.horizontalLayout.addWidget(self.pathLE)
+        self.loadBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.loadBtn.setObjectName("loadBtn")
+        self.horizontalLayout.addWidget(self.loadBtn)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.pandasTv = QtWidgets.QTableView(self.centralwidget)
+        self.pandasTv.setObjectName("pandasTv")
+        self.verticalLayout.addWidget(self.pandasTv)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 512, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.loadBtn.setText(_translate("MainWindow", "Load"))
+
+
+import pandas as pd
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent=parent)
+        self.setupUi(self)
+        self.loadBtn.clicked.connect(self.loadFile)
+
+    def loadFile(self):
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "", "CSV Files (*.csv)");
+        self.pathLE.setText(fileName)
+        df = pd.read_csv(fileName)
+        model = DataFrameModel(df)
+        self.pandasTv.setModel(model)
+
+class DataFrameModel(QtCore.QAbstractTableModel): 
+    def __init__(self, df = pd.DataFrame(), parent=None): 
+        """ datain: a list of lists
+            headerdata: a list of strings
+        """
+        QtCore.QAbstractTableModel.__init__(self, parent=parent)
+        self._df = df
+
+    def setDataFrame(self, df):
+        self._df = df;
+
+
+    #------------- table display functions -----------------
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        if role != QtCore.Qt.DisplayRole:
+            return QtCore.QVariant()
+
+        if orientation == QtCore.Qt.Horizontal:
+            try:
+                return self._df.columns.tolist()[section]
+            except (IndexError, ):
+                return QtCore.QVariant()
+        elif orientation == QtCore.Qt.Vertical:
+            try:
+                # return self.df.index.tolist()
+                return self._df.index.tolist()[section]
+            except (IndexError, ):
+                return QtCore.QVariant()
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if role != QtCore.Qt.DisplayRole:
+            return QtCore.QVariant()
+
+        if not index.isValid():
+            return QtCore.QVariant()
+
+        return QtCore.QVariant(str(self._df.ix[index.row(), index.column()]))
+
+    def flags(self, index):
+            flags = super(DataFrameModel, self).flags(index)
+            return flags
+
+    def setData(self, index, value, role):
+        row = self._df.index[index.row()]
+        col = self._df.columns[index.column()]
+        if hasattr(value, 'toPyObject'):
+            # PyQt4 gets a QVariant
+            value = value.toPyObject()
+        else:
+            # PySide gets an unicode
+            dtype = self._df[col].dtype
+            if dtype != object:
+                value = None if value == '' else dtype.type(value)
+        self._df.set_value(row, col, value)
+        return True
+
+    def rowCount(self, parent=QtCore.QModelIndex()): 
+        return len(self._df.index)
+
+    def columnCount(self, parent=QtCore.QModelIndex()): 
+        return len(self._df.columns)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
