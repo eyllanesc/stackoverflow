f940b7207533b6745cb4d16de50d8e5b5108cd21
eyllanesc <e.yllanescucho@gmail.com>
2017-09-13 21:17:46 -0500

    update

--- 46169378-qtableview-display-data-in-specific-column-based-on-value-in-other-column/main.cpp
+++ 46169378-qtableview-display-data-in-specific-column-based-on-value-in-other-column/main.cpp
@@ -22,11 +22,11 @@ static bool createConnection()
     query.exec("create table TableExample (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
                "Field1 varchar(20), position INTEGER, Field2 varchar(20), state varchar(20), Field3 varchar(20))");
 
-    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field2) values('Danny', 3. 'Young', 'A', 'aaa')");
-    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field2) values('Christine', 4,  'Holand', 'B', 'bbb')");
-    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field2) values('Lars', 1, 'Gordon', 'C', 'ccc')");
-    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field2) values('Roberto', 5, 'Robitaille', 'D', 'ddd')");
-    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field2) values('Maria', 2, 'Papadopoulos', 'E', 'eee')");
+    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field3) values('Danny', 3. 'Young', 'A', 'aaa')");
+    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field3) values('Christine', 4,  'Holand', 'B', 'bbb')");
+    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field3) values('Lars', 1, 'Gordon', 'C', 'ccc')");
+    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field3) values('Roberto', 5, 'Robitaille', 'D', 'ddd')");
+    query.exec("insert INTO TableExample (Field1, position, Field2, state, Field3) values('Maria', 2, 'Papadopoulos', 'E', 'eee')");
 
     return true;
 }
