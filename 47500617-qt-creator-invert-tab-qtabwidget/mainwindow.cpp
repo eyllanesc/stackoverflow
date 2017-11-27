64a4b75f13f227b4ccb5ca16dd086c26b0a2562b
eyllanesc <e.yllanescucho@gmail.com>
2017-11-26 21:04:50 -0500

    47500617

---
+++ 47500617-qt-creator-invert-tab-qtabwidget/mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "tabwidget.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
