4ba38adfd1c79cea3134ec022a03f41f93062f47
eyllanesc <e.yllanescucho@gmail.com>
2017-12-12 11:48:22 -0500

    mod

--- 47775985/main.cpp
+++ 47775985/main.cpp
@@ -4,15 +4,15 @@
 
 #include <QDebug>
 
-QtCharts::QLineSeries *convertLine2Step(const QtCharts::QLineSeries* series){
-    QtCharts::QLineSeries *s = new QtCharts::QLineSeries;
-    *s<<series->at(0);
+QVector<QPointF> convertLine2Step(const QVector<QPointF> &points){
+    QVector<QPointF> s;
+    s<<points.at(0);
     int x, y;
-    for(int i=1; i < series->count(); i++){
-        x = series->at(i).x();
-        y = series->at(i-1).y();
-        *s<<QPointF(x, y);
-        *s <<series->at(i);
+    for(int i=1; i < points.count(); i++){
+        x = points.at(i).x();
+        y = points.at(i-1).y();
+        s<<QPointF(x, y);
+        s <<points.at(i);
     }
     return s;
 }
@@ -26,14 +26,18 @@ int main(int argc, char *argv[])
     chart.legend()->setVisible(true);
     w.setChart(&chart);
     QtCharts::QLineSeries *series = new QtCharts::QLineSeries;
-    series->append(0, 6);
-    series->append(2, 4);
-    series->append(3, 8);
-    series->append(7, 4);
-    series->append(10, 5);
-    *series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+    QVector<QPointF> points;
+    points<<QPointF(2, 4);
+    points<<QPointF(3, 8);
+    points<<QPointF(7, 4);
+    points<<QPointF(10, 5);
+    points << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
+
+    *series<< points.toList();
     series->setName("original");
-    QtCharts::QLineSeries *stepSerie = convertLine2Step(series);
+
+    QtCharts::QLineSeries *stepSerie = new QtCharts::QLineSeries;
+    *stepSerie<<convertLine2Step(points).toList();
     stepSerie->setName("step");
     chart.addSeries(series);
     chart.addSeries(stepSerie);
