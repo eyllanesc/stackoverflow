955fdb23a27a7a4891b3616c0fa83e828f9a9ca9
eyllanesc <e.yllanescucho@gmail.com>
2018-01-17 11:53:29 -0500

    update

--- 48303751/grafica_ppu.cpp
+++ 48303751/grafica_ppu.cpp
@@ -25,7 +25,9 @@ void Grafica_PPU::paint(QPainter *painter, const QStyleOptionGraphicsItem *,
     painter->setRenderHint(QPainter::Antialiasing); //Ottengo bordi senza spingoli
     painter->setRenderHint(QPainter::TextAntialiasing); //Miglioro la lettura del testo ruotato
     painter->drawRect(boundingRect()); //Disegno il bordo di dimensioni scelte
-    painter->drawPoint(boundingRect().center());
+    painter->translate(boundingRect().center());
+    painter->rotate(-rotation());
+    painter->translate(-boundingRect().center());
     painter->drawText(boundingRect(),Qt::AlignCenter,Testo); //Creo un testo avente dimensione uguale alla dimensione del modulo, con il testo allineato al centro
 
 }
