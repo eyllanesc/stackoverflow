679302de8dbf3f1f06d921d83a25b14abfe5fc1e
eyllanesc <e.yllanescucho@gmail.com>
2018-01-31 12:30:54 -0500

    48534529

---
+++ 48534529-2/main.cpp
@@ -0,0 +1,22 @@
+#include "filemanager.h"
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+#if defined(Q_OS_WIN)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+    qmlRegisterType<FileManager>("com.eyllanesc.filemanager", 1,0, "FileManager");
+    qmlRegisterType<File>("com.eyllanesc.filemanager", 1,0, "File");
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
