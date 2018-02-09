0c7aae0682165f27427f3cc3e8e6dc5d27fdd5d2
eyllanesc <e.yllanescucho@gmail.com>
2018-02-09 14:39:25 -0500

    48711986

---
+++ 48711986/main.cpp
@@ -0,0 +1,39 @@
+#include "plotter.h"
+
+#include <QApplication>
+#include <QTimer>
+#include <QDebug>
+
+class Manager: public QObject{
+  Q_OBJECT
+public:
+    Manager(QObject *parent=0):QObject(parent){
+        QTimer *timer = new QTimer(this);
+        connect(timer, SIGNAL(timeout()), this, SLOT(on_Timeout()));
+        timer->start(10);
+        time = 0;
+    }
+signals:
+    void newPoint(const QPointF &p);
+public slots:
+    void on_Timeout(){
+        time += 0.1;
+        QPointF point(time, 5 + 5*qSin(time/20)*qExp(-time/1000));
+        emit newPoint(point);
+    }
+private:
+    float time;
+};
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    plotter w;
+    w.resize(640, 480);
+    Manager manager;
+    QObject::connect(&manager, SIGNAL(newPoint(QPointF)), &w, SLOT(add_point(QPointF)));
+    w.show();
+    return a.exec();
+}
+
+#include "main.moc"
---
+++ 48711986/plotter.h
@@ -0,0 +1,49 @@
+#ifndef PLOTTER_H
+#define PLOTTER_H
+
+#include <QObject>
+
+#include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+#include <qwt_plot_grid.h>
+#include <qwt_symbol.h>
+#include <qwt_legend.h>
+#include <qwt_plot_canvas.h>
+#include <qwt_plot_dict.h>
+#include <qwt_plot_directpainter.h>
+
+
+class CurveData : public QwtArraySeriesData<QPointF> {
+public:
+    CurveData() {}
+
+    virtual QRectF boundingRect() const {
+        if (d_boundingRect.width() < 0.0)
+            d_boundingRect = qwtBoundingRect(*this);
+
+        return d_boundingRect;
+    }
+
+    inline void append(const QPointF &point) { d_samples += point; }
+
+    void clear() {
+        d_samples.clear();
+        d_samples.squeeze();
+        d_boundingRect = QRectF(0.0, 0.0, -1.0, -1.0);
+    }
+};
+
+class plotter : public QwtPlot {
+    Q_OBJECT
+public:
+    plotter(QWidget *parent = nullptr);
+
+public slots:
+    void add_point(const QPointF &p);
+
+private:
+    QwtPlotCurve *curve{nullptr};
+    QwtPlotDirectPainter *direct_painter;
+};
+
+#endif // PLOTTER_H
