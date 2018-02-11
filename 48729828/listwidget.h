6c106db7a106717f33bd6f420ae7c8737bfeefe5
eyllanesc <e.yllanescucho@gmail.com>
2018-02-11 15:45:38 -0500

    48729828

---
+++ 48729828/listwidget.h
@@ -0,0 +1,16 @@
+#ifndef LISTWIDGET_H
+#define LISTWIDGET_H
+
+#include <QListWidget>
+
+class ListWidget : public QListWidget
+{
+    Q_OBJECT
+public:
+    ListWidget(QWidget * parent = 0);
+    void dropEvent(QDropEvent * event);
+
+protected:
+    void mouseMoveEvent(QMouseEvent * event);
+};
+#endif // LISTWIDGET_H
---
+++ 48729828/mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QComboBox>
+#include <QDir>
+#include <QDirIterator>
+#include <QFileInfoList>
+#include <QListWidget>
+
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QDir dir (":/icons");
+    QFileInfoList fileInfoList = dir.entryInfoList(QDir::AllEntries |
+                                                   QDir::Dirs|QDir::NoDotAndDotDot);
+
+    for(const QFileInfo& finfo : fileInfoList){
+        QIcon icon;
+        icon.addFile(finfo.absoluteFilePath(), QSize(), QIcon::Normal, QIcon::Off);
+        QListWidgetItem *item = new QListWidgetItem;
+        item->setIcon(icon);
+        ui->listwidget1->insertItem(ui->listwidget1->count(), item);
+        QComboBox *box = new QComboBox;
+        box->addItems(QStringList{finfo.baseName() ,"item1", "item2", "item3"});
+        ui->listwidget1->setItemWidget(item, box);
+        QSize size =item->sizeHint();
+        size.setHeight(box->sizeHint().height());
+        item->setSizeHint(size);
+    }
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
