bbba0ce27844914544ffdaaf0cbbe0dd72100fda
eyllanesc <e.yllanescucho@gmail.com>
2018-02-18 11:14:30 -0500

    48853221

---
+++ 48853221/customslider.cpp
@@ -0,0 +1,40 @@
+#include "customslider.h"
+#include "ui_customslider.h"
+
+#include <QEvent>
+#include <QSlider>
+
+CustomSlider::CustomSlider(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::CustomSlider)
+{
+    ui->setupUi(this);
+    ui->horizontalSlider->installEventFilter(this);
+    ui->horizontalSlider->setMaximum(1);
+}
+
+CustomSlider::~CustomSlider()
+{
+    delete ui;
+}
+
+bool CustomSlider::eventFilter(QObject *obj, QEvent *ev )
+{
+    if (ev->type() == QEvent::MouseButtonRelease)
+    {
+        changeSliderValue();
+    }
+    if(ev->type() == QEvent::MouseButtonPress){
+        lastSaved = ui->horizontalSlider->value();
+    }
+    return QWidget::eventFilter(obj,ev);
+}
+
+
+void CustomSlider::changeSliderValue()
+{
+    int current = ui->horizontalSlider->value();
+    if(lastSaved==current){
+        ui->horizontalSlider->setValue(current==1? 0: 1);
+    }
+}
---
+++ 48853221/customslider.h
@@ -0,0 +1,25 @@
+#ifndef CUSTOMSLIDER_H
+#define CUSTOMSLIDER_H
+
+#include <QWidget>
+
+namespace Ui {
+class CustomSlider;
+}
+
+class CustomSlider : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CustomSlider(QWidget *parent = 0);
+    ~CustomSlider();
+
+    bool eventFilter(QObject *obj, QEvent *ev);
+private:
+    Ui::CustomSlider *ui;
+    void changeSliderValue();
+    int lastSaved;
+};
+
+#endif // CUSTOMSLIDER_H
