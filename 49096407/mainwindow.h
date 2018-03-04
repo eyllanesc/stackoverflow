affbf055698808ce3b8428b419b2d60d35abb283
eyllanesc <e.yllanescucho@gmail.com>
2018-03-04 11:32:44 -0500

    49096407

---
+++ 49096407/mainwindow.h
@@ -0,0 +1,24 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTextEdit>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void newTabActionHandler();
+
+private:
+    QList<QWidget*> widgetList;
+    QList<QTextEdit*> textEditList;
+    QTabWidget* tabWidget;
+};
+
+#endif // MAINWINDOW_H
