718334d021ab2c83536b2550e51f03c0962bf3a7
eyllanesc <e.yllanescucho@gmail.com>
2018-03-09 12:24:22 -0500

    49197080

---
+++ 49197080/testsize.h
@@ -0,0 +1,24 @@
+#ifndef TESTSIZE_H
+#define TESTSIZE_H
+
+#include <QWidget>
+
+class QLabel;
+
+class testsize : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit testsize(QWidget *parent = 0);
+    ~testsize();
+
+private:
+    QLabel *image;
+    QPixmap original_px;
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+};
+
+#endif // TESTSIZE_H
