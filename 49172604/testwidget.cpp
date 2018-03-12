f8d7fd43242edf7cce775001eb8461c0622de5f7
eyllanesc <e.yllanescucho@gmail.com>
2018-03-12 17:43:07 -0500

    49172604

---
+++ 49172604/testwidget.cpp
@@ -0,0 +1,16 @@
+#include "testwidget.h"
+
+TestWidget::TestWidget(QWidget *parent) :
+    QWidget(parent)
+{
+}
+
+TestWidget::EnumTest TestWidget::test() const
+{
+    return mTest;
+}
+
+void TestWidget::setTest(const EnumTest &test)
+{
+    mTest = test;
+}
---
+++ 49172604/testwidgetplugin.cpp
@@ -0,0 +1,73 @@
+#include "testwidget.h"
+#include "testwidgetplugin.h"
+
+#include <QtPlugin>
+
+TestWidgetPlugin::TestWidgetPlugin(QObject *parent)
+    : QObject(parent)
+{
+    m_initialized = false;
+}
+
+void TestWidgetPlugin::initialize(QDesignerFormEditorInterface * /* core */)
+{
+    if (m_initialized)
+        return;
+
+    // Add extension registrations, etc. here
+
+    m_initialized = true;
+}
+
+bool TestWidgetPlugin::isInitialized() const
+{
+    return m_initialized;
+}
+
+QWidget *TestWidgetPlugin::createWidget(QWidget *parent)
+{
+    return new TestWidget(parent);
+}
+
+QString TestWidgetPlugin::name() const
+{
+    return QLatin1String("TestWidget");
+}
+
+QString TestWidgetPlugin::group() const
+{
+    return QLatin1String("TestWidgetGroup");
+}
+
+QIcon TestWidgetPlugin::icon() const
+{
+    return QIcon(QLatin1String(":/if_Sharethis_322291.png"));
+}
+
+QString TestWidgetPlugin::toolTip() const
+{
+    return QLatin1String("I am TestWidget");
+}
+
+QString TestWidgetPlugin::whatsThis() const
+{
+    return QLatin1String("");
+}
+
+bool TestWidgetPlugin::isContainer() const
+{
+    return false;
+}
+
+QString TestWidgetPlugin::domXml() const
+{
+    return QLatin1String("<widget class=\"TestWidget\" name=\"testWidget\">\n</widget>\n");
+}
+
+QString TestWidgetPlugin::includeFile() const
+{
+    return QLatin1String("testwidget.h");
+}
+#if QT_VERSION < 0x050000
+Q_EXPORT_PLUGIN2(testwidgetplugin, TestWidgetPlugin)
+#endif // QT_VERSION < 0x050000
