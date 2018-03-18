cc486267d9fdde7bc019b1b1ff85cf6710a77c33
eyllanesc <e.yllanescucho@gmail.com>
2018-03-18 01:58:13 -0500

    49344638

---
+++ 49344638/markermodel.h
@@ -0,0 +1,44 @@
+#ifndef MARKERMODEL_H
+#define MARKERMODEL_H
+
+#include <QAbstractListModel>
+#include <QGeoCoordinate>
+
+class MarkerModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    using QAbstractListModel::QAbstractListModel;
+    enum MarkerRoles{positionRole = Qt::UserRole + 1};
+
+    Q_INVOKABLE void addMarker(const QGeoCoordinate &coordinate){
+        beginInsertRows(QModelIndex(), rowCount(), rowCount());
+        m_coordinates.append(coordinate);
+        endInsertRows();
+    }
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override{
+        Q_UNUSED(parent)
+        return m_coordinates.count();
+    }
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override{
+        if (index.row() < 0 || index.row() >= m_coordinates.count())
+            return QVariant();
+        if(role== MarkerModel::positionRole)
+            return QVariant::fromValue(m_coordinates[index.row()]);
+        return QVariant();
+    }
+
+    QHash<int, QByteArray> roleNames() const{
+        QHash<int, QByteArray> roles;
+        roles[positionRole] = "position";
+        return roles;
+    }
+
+private:
+    QList<QGeoCoordinate> m_coordinates;
+};
+
+#endif // MARKERMODEL_H
