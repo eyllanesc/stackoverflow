758c56d478333a3c7918f2b7f0e772be9d735fbf
eyllanesc <e.yllanescucho@gmail.com>
2018-04-02 02:13:08 -0500

    49605955

---
+++ 49605955/book.cpp
@@ -0,0 +1,42 @@
+#include "book.h"
+
+Book::Book()
+{
+    Book("", "", "");
+}
+
+Book::Book(const std::string &author, const std::string &name, const std::string &uuid):
+   mAuthor(author), mName(name), mUuid(uuid)
+{
+
+}
+
+std::string Book::getAuthor() const
+{
+    return mAuthor;
+}
+
+void Book::setAuthor(const std::string &author)
+{
+    mAuthor = author;
+}
+
+std::string Book::getName() const
+{
+    return mName;
+}
+
+void Book::setName(const std::string &name)
+{
+    mName = name;
+}
+
+std::string Book::getUid() const
+{
+    return mUuid;
+}
+
+void Book::setUid(const std::string &uuid)
+{
+    mUuid = uuid;
+}
---
+++ 49605955/library.cpp
@@ -0,0 +1,22 @@
+#include "library.h"
+
+Library::Library()
+{
+
+}
+
+void Library::addBook(const Book &book)
+{
+    books.push_back(book);
+}
+
+Book Library::getBook(int index)
+{
+    return books[index];
+
+}
+
+int Library::numOfBooks()
+{
+    return books.size();
+}
