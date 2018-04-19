03c7ba8124f4840f7fd502e6bef5a1d45227bc68
eyllanesc <e.yllanescucho@gmail.com>
2018-04-18 21:48:25 -0500

    49910514

---
+++ 49910514/qgraphicspolygonitem.cpp
@@ -0,0 +1,20 @@
+#include "graphicsproxywidget.h"
+#include "qgraphicspolygonitem.h"
+
+#include <QLabel>
+
+DiagramItem::DiagramItem(QGraphicsItem *parent):QGraphicsPolygonItem(parent) {
+    label = new QLabel;
+    pMyProxy = new GraphicsProxyWidget(this);
+    pMyProxy->setWidget(label);
+    label->setTextInteractionFlags(Qt::TextEditorInteraction);
+    label->setStyleSheet("QLabel { background-color : red; color : blue; }");
+    setText(QString("I AM A SQARE DADADA"));
+    setBrush(Qt::green);
+}
+
+void DiagramItem::setText(const QString &text){
+    label->setText(text);
+    label->adjustSize();
+    pMyProxy->setPos(boundingRect().center()-label->rect().center());
+}
---
+++ 49910514/qgraphicspolygonitem.h
@@ -0,0 +1,17 @@
+#ifndef QGRAPHICSPOLYGONITEM_H
+#define QGRAPHICSPOLYGONITEM_H
+
+#include <QGraphicsPolygonItem>
+
+class QLabel;
+class GraphicsProxyWidget;
+
+class DiagramItem: public QGraphicsPolygonItem{
+    QLabel *label;
+    GraphicsProxyWidget *pMyProxy ;
+public:
+    explicit DiagramItem(QGraphicsItem *parent=nullptr);
+    void setText(const QString & text);
+};
+
+#endif // QGRAPHICSPOLYGONITEM_H
