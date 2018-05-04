88672b090cdb73a4b7a1fa0bfd6968c54a67eeac
eyllanesc <e.yllanescucho@gmail.com>
2018-05-04 16:40:52 -0500

    50180990

---
+++ 50180990/main.cpp
@@ -0,0 +1,38 @@
+#include <QApplication>
+#include <QFileDialog>
+#include <QQmlApplicationEngine>
+#include <QQmlEngine>
+#include <QtWebEngine>
+
+class DownloadHelper: public QObject{
+    Q_OBJECT
+public:
+    Q_INVOKABLE void onDownloadRequested(QObject * download){
+        const QString & old_path = download->property("path").toString();
+        const QString & suffix = QFileInfo(old_path).suffix();
+        const QString & path  = QFileDialog::getSaveFileName(nullptr, "Save File", old_path, "*."+suffix);
+        if(!path.isEmpty()){
+            download->setProperty("path", path);
+            Q_ASSERT(QMetaObject::invokeMethod(download, "accept"));
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QApplication app(argc, argv);
+    QtWebEngine::initialize();
+
+    QQmlApplicationEngine engine;
+    DownloadHelper helper;
+    engine.rootContext()->setContextProperty("downloadHelper", &helper);
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
+
+#include "main.moc"
