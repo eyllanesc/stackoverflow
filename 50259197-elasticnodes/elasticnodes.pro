04c15d597c4cc8ed8f0f39fa2398f3b089ceada7
eyllanesc <e.yllanescucho@gmail.com>
2018-05-09 13:38:22 -0500

    50259197

---
+++ 50259197-elasticnodes/elasticnodes.pro
@@ -0,0 +1,15 @@
+QT += widgets
+
+HEADERS += \
+        edge.h \
+        node.h \
+        graphwidget.h \
+    win.h
+
+SOURCES += \
+        edge.cpp \
+        main.cpp \
+        node.cpp \
+        graphwidget.cpp \
+    win.cpp
+
---
+++ 50259197-elasticnodes/graphwidget.h
@@ -0,0 +1,41 @@
+#ifndef GRAPHWIDGET_H
+#define GRAPHWIDGET_H
+
+#include <QGraphicsView>
+#include "edge.h"
+#include "node.h"
+
+#include <math.h>
+
+#include <QKeyEvent>
+#include <QRandomGenerator>
+
+class Node;
+
+class GraphWidget : public QGraphicsView
+{
+    Q_OBJECT
+
+public:
+    GraphWidget(QWidget *parent = 0);
+
+    void itemMoved();
+
+public slots:
+    void addNode();
+    void shuffle();
+    void zoomIn();
+    void zoomOut();
+
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+#if QT_CONFIG(wheelevent)
+    void wheelEvent(QWheelEvent *event) override;
+#endif
+    void scaleView(qreal scaleFactor);
+
+private:
+};
+
+#endif // GRAPHWIDGET_H
