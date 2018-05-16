3186d52fb6d782e788e28c42f2b34ffa68aadd20
eyllanesc <e.yllanescucho@gmail.com>
2018-05-16 15:51:18 -0500

    50378537

---
+++ 50378537/demopromotedwidget.cpp
@@ -0,0 +1,14 @@
+#include "demopromotedwidget.h"
+#include "ui_demopromotedwidget.h"
+
+DemoPromotedWidget::DemoPromotedWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::DemoPromotedWidget)
+{
+    ui->setupUi(this);
+}
+
+DemoPromotedWidget::~DemoPromotedWidget()
+{
+    delete ui;
+}
