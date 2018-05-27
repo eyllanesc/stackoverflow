7ca1aaca204f3b72446f9aae69aab6630c3e03a4
eyllanesc <e.yllanescucho@gmail.com>
2018-05-27 05:23:40 -0500

    to QSequentialAnimationGroup

--- 50550089/main.cpp
+++ 50550089/main.cpp
@@ -10,29 +10,25 @@ int main(int argc, char *argv[])
     QFrame frame;
     frame.resize(320, 240);
     QSequentialAnimationGroup group;
+    group.setLoopCount(-1);
     int minSize = 5;
     int maxSize = 30;
     int labelSize = 50;;
     for(const QPoint & pos: {QPoint(0, 0), QPoint(0, 40), QPoint(0, 80)}){
+        QRect startVal = QRect(pos + (QPoint(labelSize, labelSize) + QPoint(-minSize, -minSize))/2 , QSize(minSize, minSize));
+        QRect endVal = QRect(pos + (QPoint(labelSize, labelSize) + QPoint(-maxSize, -maxSize))/2 , QSize(maxSize, maxSize));
         QLabel *label = new QLabel(&frame);
-        label->resize(labelSize, labelSize);
-        label->move(pos);
+        label->setGeometry(startVal);
         label->setPixmap(QPixmap(":/circle.png"));
         label->setScaledContents(true);
         label->setAlignment(Qt::AlignCenter);
         QPropertyAnimation *animation = new QPropertyAnimation(label, "geometry");
-        QRect startVal = QRect(pos + (QPoint(labelSize, labelSize) + QPoint(-minSize, -minSize))/2 , QSize(minSize, minSize));
-        QRect endVal = QRect(pos + (QPoint(labelSize, labelSize) + QPoint(-maxSize, -maxSize))/2 , QSize(maxSize, maxSize));
         animation->setStartValue(startVal);
-        animation->setEndValue(endVal);
+        animation->setKeyValueAt(.5, endVal);
+        animation->setEndValue(startVal);
+        animation->setDuration(1000);
         group.addAnimation(animation);
     }
-    QObject::connect(&group, &QAbstractAnimation::finished, [&group](){
-        QAbstractAnimation::Direction direction = group.direction() == QAbstractAnimation::Forward ?
-                    QAbstractAnimation::Backward : QAbstractAnimation::Forward;
-        group.setDirection(direction);
-        group.start();
-    });
     group.start();
     frame.show();
     return a.exec();
