aef1552b847561a02bc2a170537a0c7ef3fabc39
eyllanesc <e.yllanescucho@gmail.com>
2018-05-27 03:54:51 -0500

    50550089

---
+++ 50550089/main.cpp
@@ -0,0 +1,39 @@
+#include <QApplication>
+#include <QFrame>
+#include <QLabel>
+#include <QParallelAnimationGroup>
+#include <QPropertyAnimation>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QFrame frame;
+    frame.resize(320, 240);
+    QParallelAnimationGroup group;
+    int minSize = 5;
+    int maxSize = 30;
+    int labelSize = 50;;
+    for(const QPoint & pos: {QPoint(0, 0), QPoint(0, 40), QPoint(0, 80)}){
+        QLabel *label = new QLabel(&frame);
+        label->resize(labelSize, labelSize);
+        label->move(pos);
+        label->setPixmap(QPixmap(":/circle.png"));
+        label->setScaledContents(true);
+        label->setAlignment(Qt::AlignCenter);
+        QPropertyAnimation *animation = new QPropertyAnimation(label, "geometry");
+        QRect startVal = QRect(pos + (QPoint(labelSize, labelSize) + QPoint(-minSize, -minSize))/2 , QSize(minSize, minSize));
+        QRect endVal = QRect(pos + (QPoint(labelSize, labelSize) + QPoint(-maxSize, -maxSize))/2 , QSize(maxSize, maxSize));
+        animation->setStartValue(startVal);
+        animation->setEndValue(endVal);
+        group.addAnimation(animation);
+    }
+    QObject::connect(&group, &QParallelAnimationGroup::finished, [&group](){
+        QAbstractAnimation::Direction direction = group.direction() == QAbstractAnimation::Forward ?
+                    QAbstractAnimation::Backward : QAbstractAnimation::Forward;
+        group.setDirection(direction);
+        group.start();
+    });
+    group.start();
+    frame.show();
+    return a.exec();
+}
