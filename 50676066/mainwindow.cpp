fa8d0431b82a3fe599d2f798b4ac39b5d4e1f63d
eyllanesc <e.yllanescucho@gmail.com>
2018-06-04 11:36:40 -0500

    50676066

---
+++ 50676066/mainwindow.cpp
@@ -0,0 +1,47 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QKeyEvent>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //scene
+    scene = new QGraphicsScene(0, 0, 1050, 600);
+    //player
+    player = new QGraphicsPixmapItem(QPixmap(":/img/whitedog.png").scaled(100,100));
+    player->setPos(350, 500);
+    scene->addItem(player);
+    //view
+    view = new QGraphicsView;
+    view ->setScene(scene);
+    view->installEventFilter(this);
+    setCentralWidget(view);
+    view->setFixedSize(1055,605);
+}
+
+bool MainWindow::eventFilter(QObject *watched, QEvent *event)
+{
+    if(watched == view && event->type() == QEvent::KeyPress){
+        QKeyEvent *kevent = static_cast<QKeyEvent *>(event);
+        if(kevent->key() == Qt::Key_Down){
+            player->setPos(player->pos() + QPointF(0, 10));
+            return true;
+        }
+        if(kevent->key() == Qt::Key_Up){
+            player->setPos(player->pos() - QPointF(0, 10));
+            return true;
+        }
+    }
+    return QMainWindow::eventFilter(watched, event);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
