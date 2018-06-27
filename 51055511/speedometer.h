3bec23be9ed3a4a0a4ee5935c0cee51a650157fa
eyllanesc <e.yllanescucho@gmail.com>
2018-06-27 02:02:39 -0500

    51055511

---
+++ 51055511/speedometer.h
@@ -0,0 +1,21 @@
+#ifndef SPEEDOMETER_H
+#define SPEEDOMETER_H
+
+#include <qwt_dial.h>
+
+class SpeedoMeter : public QwtDial
+{
+public:
+    SpeedoMeter(QWidget *parent=nullptr);
+
+protected:
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
+    void dragMoveEvent(QDragMoveEvent *event);
+    void paintEvent(QPaintEvent *event);
+private:
+    QString d_label;
+};
+
+
+#endif // SPEEDOMETER_H
