0500bb47c4482ff8925de7a728005f3a4dd6d700
eyllanesc <e.yllanescucho@gmail.com>
2018-07-03 04:24:15 -0500

    51149966

---
+++ 51149966/samplemanager.h
@@ -0,0 +1,28 @@
+#ifndef SAMPLEMANAGER_H
+#define SAMPLEMANAGER_H
+
+#include "samplemodel.h"
+
+#include <QObject>
+
+class SampleManager : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(SampleModel* model READ model WRITE setModel NOTIFY modelChanged)
+public:
+    using QObject::QObject;
+    SampleModel *model() const{
+        return mModel.get();
+    }
+    void setModel(SampleModel *model){
+        if(mModel.get() == model)
+            return;
+        mModel.reset(model);
+    }
+signals:
+    void modelChanged();
+private:
+    QScopedPointer<SampleModel> mModel;
+};
+
+#endif // SAMPLEMANAGER_H
