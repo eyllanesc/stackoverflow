6240c0c3741df135bcdc4c0278186b09d046702d
eyllanesc <e.yllanescucho@gmail.com>
2018-08-01 05:44:58 -0500

    51629014

---
+++ 51629014/appendproxymodel.cpp
@@ -0,0 +1,39 @@
+#include "appendproxymodel.h"
+
+QVariant AppendProxyModel::data(const QModelIndex &index, int role) const
+{
+    if(index.row() >= 0 && index.row() < mDefaults.size() && role==Qt::DisplayRole){
+        return mDefaults[index.row()];
+    }
+    if(index.row() >= mDefaults.size() && index.row() < rowCount()){
+        return  QIdentityProxyModel::data(
+                    QIdentityProxyModel::index(index.row()-mDefaults.size(), index.column()),
+                    role);
+    }
+    return QIdentityProxyModel::data(index, role);
+}
+
+int AppendProxyModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    int rc = sourceModel() ? sourceModel()->rowCount() : 0;
+    return  mDefaults.size()+ rc;
+}
+
+QModelIndex AppendProxyModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if(row < rowCount()- mDefaults.size())
+        QIdentityProxyModel::index(row, column, parent);
+    return createIndex(row, column);
+}
+
+QStringList AppendProxyModel::defaults() const
+{
+    return mDefaults;
+}
+
+void AppendProxyModel::setDefaults(const QStringList &defaults)
+{
+    mDefaults = defaults;
+    emit dataChanged(index(0, 0, QModelIndex()), index(rowCount(),columnCount(), QModelIndex()));
+}
