688dbfa9094b603bc6bcac908cdde2992019b4e2
eyllanesc <e.yllanescucho@gmail.com>
2018-08-08 08:44:03 -0500

    51747950

---
+++ 51747950/custom_class.h
@@ -0,0 +1,37 @@
+#ifndef CUSTOM_CLASS_H
+#define CUSTOM_CLASS_H
+
+#include <QObject>
+
+class CustomClass : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName)
+public:
+    CustomClass(QObject* parent = nullptr)
+        : QObject(parent)
+    {}
+
+    CustomClass(QString name) {
+        name_ = name;
+    }
+
+    CustomClass(const CustomClass& item) {
+        name_ = item.name_;
+    }
+
+    QString name() const {
+        return name_;
+    }
+
+    void setName(QString name) {
+        name_ = name;
+    }
+
+private:
+    QString name_;
+};
+
+Q_DECLARE_METATYPE(CustomClass)
+
+#endif // CUSTOM_CLASS_H
---
+++ 51747950/main.cpp
@@ -0,0 +1,20 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "backend.h"
+#include "custom_class.h"
+
+void register_Qml_types() {
+  qmlRegisterType<Backend>("customApi", 1, 0, "Backend");
+  qmlRegisterType<CustomClass>("customApi", 1, 0, "CustomClass");
+}
+
+int main(int argc, char **argv)
+{
+  QGuiApplication app(argc, argv);
+
+  register_Qml_types();
+  QQmlApplicationEngine engine;
+  engine.load(QUrl("qrc:/main.qml"));
+
+  return app.exec();
+}
