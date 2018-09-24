777b1a3df6030a56d766bd64fe2d32253b3331a0
eyllanesc <e.yllanescucho@gmail.com>
2018-09-24 12:58:46 -0500

    test-x-special/nautilus-clipboard

---
+++ 52483937/userentrywidget.cpp
@@ -0,0 +1,28 @@
+#include "userentrywidget.h"
+
+#include <QHBoxLayout>
+#include <QLabel>
+
+UserEntryWidget::UserEntryWidget(QWidget* parent) : QWidget(parent)
+{
+   avatar = new QLabel();
+   title = new QLabel("title");
+   subtitle = new QLabel("subtitle");
+   time = new QLabel("00:00");
+   auto layout = new QHBoxLayout(this);
+   layout->addWidget(avatar);
+   auto centralColumn = new QVBoxLayout();
+   centralColumn->addWidget(title);
+   centralColumn->addWidget(subtitle);
+   layout->addLayout(centralColumn);
+   layout->addWidget(time);
+   setFixedHeight(sizeHint().height());
+}
+
+void UserEntryWidget::setUserInfo(const UserInfo& user)
+{
+   avatar->setPixmap(QPixmap::fromImage(user.getAvatar()));
+   title->setText(user.getTitle());
+   subtitle->setText(user.getSubtitle());
+   time->setText(user.getTime().toString("hh:mm"));
+}
