8ade5598af9c75bc05683899e3dc154ab3e111cf
eyllanesc <e.yllanescucho@gmail.com>
2018-10-28 09:14:05 -0500

    53018492

---
+++ 53018492/flightroute.h
@@ -0,0 +1,39 @@
+#ifndef FLIGHTROUTE_H
+#define FLIGHTROUTE_H
+
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QObject>
+struct FPlanWPT {
+    QString name;
+    double longitude;
+    double latitude;
+    double bearing;
+    QJsonObject toObject() const{
+        QJsonObject obj;
+        obj["name"] = name;
+        obj["longitude"] = longitude;
+        obj["latitude"] = latitude;
+        obj["bearing"] = bearing;
+        return obj;
+    }
+};
+
+
+class FlightRoute : public QObject
+{
+    Q_OBJECT
+public:
+    using QObject::QObject;
+    void setRoutes(const QList<FPlanWPT> &routes){
+        QJsonArray array;
+        for(const FPlanWPT & route: routes){
+            array.append(route.toObject());
+        }
+        emit routesChanged(array);
+    }
+signals:
+    void routesChanged(const QJsonArray &);
+};
+
+#endif // FLIGHTROUTE_H
---
+++ 53018492/main.cpp
@@ -0,0 +1,38 @@
+#include "flightroute.h"
+
+#include <QApplication>
+#include <QTimer>
+#include <QWebChannel>
+#include <QWebEngineView>
+#include <random>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QTWEBENGINE_REMOTE_DEBUGGING", "9000");
+    QApplication a(argc, argv);
+    QWebEngineView view;
+
+    FlightRoute routeIPC;
+    QWebChannel webChannel;
+    webChannel.registerObject(QStringLiteral("routeIPC"), &routeIPC);
+    view.page()->setWebChannel(&webChannel);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(0, 100);
+
+    QTimer timer;
+    QObject::connect(&timer, &QTimer::timeout, [&](){
+        QList<FPlanWPT> routes;
+        for(int i=0; i<10; i++){
+            routes << FPlanWPT{"name1", dist(gen), dist(gen), dist(gen)};
+        }
+        routeIPC.setRoutes(routes);
+    });
+    timer.start(1000);
+
+    view.load(QUrl(QStringLiteral("qrc:/index.html")));
+    view.resize(640, 480);
+    view.show();
+    return a.exec();
+}
