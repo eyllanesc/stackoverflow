fd9e4c874f9e11cf8555cc53667f93d1970a1810
eyllanesc <e.yllanescucho@gmail.com>
2018-11-13 18:51:50 -0500

    53288644

---
+++ questions/53288644/mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include <iostream>
+#include <QDebug>
+
+Worker::Worker() : t(this)
+{
+    connect(&t, &QTimer::timeout, this, &Worker::process);
+}
+
+void Worker::process()
+{
+    std::cout << "triggering timer" << std::endl;
+}
+
+void Worker::startWorker()
+{
+    t.start(1000);
+}
+
+void Worker::stopProcess()
+{
+   QMetaObject::invokeMethod(&t, "stop", Qt::QueuedConnection);
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    wt.moveToThread(&workerThread);
+    connect(&workerThread, &QThread::started, &wt, &Worker::startWorker);
+    connect(&workerThread, &QThread::finished, &workerThread, &QObject::deleteLater);
+    workerThread.start();
+}
+
+MainWindow::~MainWindow()
+{
+    wt.stopProcess();
+    workerThread.quit();
+    workerThread.wait();
+}
---
+++ questions/53288644/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QThread>
+#include <QTimer>
+
+class Worker : public QObject
+{
+    Q_OBJECT
+public:
+    Worker();
+private:
+    QTimer t;
+public slots:
+    void process();
+    void startWorker();
+    void stopProcess();
+};
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private:
+    QThread workerThread;
+    Worker wt;
+};
+
+#endif // MAINWINDOW_H
