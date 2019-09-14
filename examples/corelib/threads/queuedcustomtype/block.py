bb27169dc755f3eebfa0ab5cea98df8f9bade0d9
eyllanesc <e.yllanescucho@gmail.com>
2019-09-13 20:49:15 -0500

    move Qt Examples to https://github.com/eyllanesc/Qute

--- examples/corelib/threads/queuedcustomtype/block.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtCore, QtGui
-
-from dataclasses import dataclass
-
-
-@dataclass
-class Block:
-    rect: QtCore.QRect
-    color: QtGui.QColor
--- examples/widgets/graphicsview/basicgraphicslayouts/layoutitem.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class LayoutItem(QtWidgets.QGraphicsLayoutItem, QtWidgets.QGraphicsItem):
-	def __init__(self, parent=None):
-		QtWidgets.QGraphicsLayoutItem.__init__()
-		QtWidgets.QGraphicsItem(parent)
-		self.m_pix = 
