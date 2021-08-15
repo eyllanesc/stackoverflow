0115c215fb1a672d56c8c8792967f31f54b8bd35
eyllanesc <e.yllanescucho@gmail.com>
2021-08-15 18:15:34 -0500

    68795702

---
+++ questions/68795702/3rdParty/OpenSAEJ1939/Hardware/Memory/FLASH_EEPROM_RAM_Memory.h
@@ -0,0 +1,27 @@
+/*
+ * FLASH_EEPROM_RAM_Memory.h
+ *
+ *  Created on: 19 juli 2021
+ *      Author: Daniel Mårtensson
+ */
+
+#ifndef HARDWARE_MEMORY_FLASH_EEPROM_RAM_MEMORY_H_
+#define HARDWARE_MEMORY_FLASH_EEPROM_RAM_MEMORY_H_
+
+/* C standard library */
+#include "stdint.h"
+
+#include "../../SAE_J1939/SAE_J1939_Enums/Enum_DM14_DM15.h"
+#include "../Processor_Choice.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void FLASH_EEPROM_RAM_Memory(uint16_t *number_of_requested_bytes, uint8_t pointer_type, uint8_t *command, uint32_t *pointer, uint8_t *pointer_extension, uint16_t *key, uint8_t raw_binary_data[]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HARDWARE_MEMORY_FLASH_EEPROM_RAM_MEMORY_H_ */
---
+++ questions/68795702/main.cpp
@@ -0,0 +1,31 @@
+#include <QApplication>
+#include <QWidget>
+
+#include <ISO_11783/ISO_11783-7_Application_Layer/Application_Layer.h>
+#include <Open_SAE_J1939/Open_SAE_J1939.h>
+#include <SAE_J1939/SAE_J1939-71_Application_Layer/Application_Layer.h>
+#include <SAE_J1939/SAE_J1939-73_Diagnostics_Layer/Diagnostics_Layer.h>
+#include <SAE_J1939/SAE_J1939-81_Network_Management_Layer/Network_Management_Layer.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    /* Create our J1939 structure with two ECU */
+    J1939 j1939_1 = {0};
+    J1939 j1939_2 = {0};
+
+    /* Important to sent all non-address to 0xFF - Else we cannot use ECU address 0x0 */
+    for(uint8_t i = 0; i < 255; i++){
+        j1939_1.other_ECU_address[i] = 0xFF; /* 0xFF is the broadcast address */
+        j1939_2.other_ECU_address[i] = 0xFF;
+    }
+
+    /* Set the ECU address */
+    j1939_1.this_ECU_address = 0x80;												/* From 0 to 253 because 254 = error address and 255 = broadcast address */
+    j1939_2.this_ECU_address = 0x90;
+
+    QWidget widget;
+    widget.show();
+    return a.exec();
+}
